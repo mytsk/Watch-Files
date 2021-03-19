fcfd67e001571a1a44ada855f62f464cc589fe9b
mytsk <mn.nilsson@gmail.com>
2021-03-19 10:42:40 +0100

    first commit

---
+++ Watch-Files.ps1
@@ -0,0 +1,65 @@
+
+function Watch-Files
+{ 
+<#  
+  .SYNOPSIS   
+    Displays file content from multiple files at the same time.
+  .DESCRIPTION
+	Reads data from one or multiple files based on your filter and your defined interval in current folder.
+  .PARAMETER Interval
+	How often to check file in seconds, default value is 2 seconds.
+  .PARAMETER Filter
+	File Filter, Mandatory, You can define multiple files in order no default value. You
+  .PARAMETER Last, default value is 5 rows.
+	Rows from each file to be displayed
+  .PARAMETER Top, default value is 5 rows.
+	Limits how many files you want to look at. 
+  .PARAMETER KeepScreenBuffer, switch
+	If defined, instead of clearing screen buffer it will continue with next itteration (without clearing).
+  .EXAMPLE  
+	Watch-Files -interval 10 -filter *.log
+   .EXAMPLE  
+	Watch-Files -interval 10 -filter *.log -last 2
+  .EXAMPLE  
+	Watch-Files -filter Agent*Trasform*.log, *.txt -Top 4 -KeepScreenBuffer 
+  .NOTES
+	Version:	1.0
+	Author:		mytsk
+	Creation Date:	2020-08-28
+	Purpose/Change: Initial development
+  .LINK
+	https:/www.github.com/mytsk/Watch-Files
+#> 
+Param(
+[parameter(position=0,Mandatory=$true)]$Filter,
+[parameter(position=1,Mandatory=$false)][int]$Interval,
+[parameter(position=2,Mandatory=$false)][int]$Last,
+[parameter(position=3,Mandatory=$false)][int]$Top,
+[parameter(position=4,Mandatory=$false)][switch]$KeepScreenBuffer
+)
+
+  if(!$Interval) {$Interval = 2}
+  if(!$Last) {$Last = 5 }
+	if(!$Top) {$Top = 10}
+	
+  while (1) {
+    if(!$KeepScreenBuffer) { clear-host }
+      Write-Host -ForegroundColor Gray "Reading from files " -nonewline       
+      Write-Host -ForegroundColor White "$filter" -nonewline     
+      Write-Host -ForegroundColor Gray " every " -nonewline 
+      Write-Host -ForegroundColor White "$interval" -nonewline 
+      Write-Host -ForegroundColor Gray " second." 
+      Write-Host -ForegroundColor DarkCyan "-------------------------------------------------------------------------------"
+      $files = Get-ChildItem -File $filter  | Sort-Object -Property LastWriteTime, Name | Select-Object -last $Top
+
+      foreach ($data in $files){
+        write-host -ForegroundColor DarkCyan "File: " -NoNewline
+        write-host -ForegroundColor Yellow $data
+        get-content -last $last $data
+      }
+      Write-Host -ForegroundColor DarkCyan "-------------------------------------------------------------------------------"
+      Write-Verbose "Files read: $files"
+      write-Host "(Ctrl + C to exit)"
+      Start-Sleep $interval
+    }
+}
